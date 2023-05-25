9053ff9d4458b64197be606ce1b62f355f025589
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 07:46:47 -0400

    recent, following tabs implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { type NextPage } from "next";
 import { NewTweetForm } from "~/components/NewTweetForm";
 import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 import { api } from "~/utils/api";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 
 // get rid of rest of imports?
@@ -10,13 +12,33 @@ import { api } from "~/utils/api";
 // import Head from "next/head";
 // import { api } from "~/utils/api";
 
+
+const TABS = ["Recent", "Following"] as const
+
 const Home: NextPage = () => {
+  const [selectedTab, setSelectedTab] = 
+  useState<(typeof TABS)[number]>("Recent");
+  const session = useSession();
 
   return (
     
     <>
     <header className="sticky top-0 z-10 border-b bg-white pt-2">
       <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+      {session.status === "authenticated" && (
+        <div className="flex">{TABS.map(
+          tab => {
+          return <button key={tab} className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${tab === selectedTab 
+            ? "border-b-4 border-b-blue-500 font-bold"
+          : ""
+        }`}
+        onClick={() => setSelectedTab(tab)}
+        >
+          {tab}
+        </button>
+        })}
+        </div>
+      )}
     </header>
     <NewTweetForm />
     <RecentTweets />
