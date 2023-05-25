3440391a30540c3d151241bfebb95a1c03d0ddb9
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 11:00:14 -0400

    profile page wip. infinite profile feed; data fetching without refresh

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -7,17 +7,19 @@ import Link from "next/link";
 import { IconHoverEffect } from "~/components/IconHoverEffect";
 import { VscArrowLeft } from "react-icons/vsc";
 import { ProfileImage } from "~/components/ProfileImage";
+import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 
 const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
     id
 }) => {
     const { data: profile } = api.profile.getById.useQuery({ id });
+    const tweets = api.tweet.infiniteProfileFeed.useInfiniteQuery({ userId: id }, { getNextPageParam: (lastPage) => lastPage.nextCursor })
 
-    if (profile == null || profile.name == null) 
+    if (profile == null || profile.name == null)
         return (
             <ErrorPage statusCode={404} />
         );
-    
+
     return (
         <>
             <Head>
@@ -25,9 +27,9 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
             </Head>
             <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
                 <Link href=".." className="mr-2">
-                <IconHoverEffect>
-                    <VscArrowLeft className="h-6 w-6" />
-                </IconHoverEffect>
+                    <IconHoverEffect>
+                        <VscArrowLeft className="h-6 w-6" />
+                    </IconHoverEffect>
                 </Link>
                 <ProfileImage src={profile.image} className="flex-shrink-0" />
                 <div className="ml-2 flex-grow">
@@ -40,13 +42,26 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
                         {profile.followersCount} {" "}
                         {getPlural(profile.followsCount, "Follower", "Followers")} - {" "}
                         {profile.followsCount} Following
-                        </div>
-                        
+                    </div>
                 </div>
-            </header>
+                    <FollowButton isFollowing={profile.isFollowing} userId={id} onClick={() => null} />
+                    </header>
+                <main>
+                    <InfiniteTweetList
+                      tweets={tweets.data?.pages.flatMap((page) => page.tweets)} 
+                      isError={tweets.isError}
+                      isLoading={tweets.isLoading}
+                      hasMore={tweets.hasNextPage}
+                      fetchNewTweets={tweets.fetchNextPage}
+                    />
+                </main>
         </>
     );
 };
+
+function FollowButton() {
+    return <h1>Follow</h1>
+}
 
 const pluralRules = new Intl.PluralRules()
 
@@ -63,7 +78,7 @@ export const getStaticPaths: GetStaticPaths = () => {
 
 export async function getStaticProps(
     context: GetStaticPropsContext<{ id: string }>
-    ) {
+) {
     const id = context.params?.id;
     if (id == null) {
         return {
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -9,6 +9,25 @@ import {
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
+  infiniteProfileFeed: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
+      }))
+    .query(
+      async ({
+        input: { limit = 10, userId, cursor }, ctx }) => {
+        return await getInfiniteTweets({
+          limit,
+          ctx,
+          cursor,
+          whereClause: { userId },
+        });
+      }),
+
+
   infiniteFeed: publicProcedure
     .input(
       z.object({
@@ -24,36 +43,40 @@ export const tweetRouter = createTRPCRouter({
       }) => {
         const currentUserId = ctx.session?.user.id
         return await getInfiniteTweets({
-          limit, ctx, cursor, whereClause: currentUserId == null || !onlyFollowing ? undefined : {
-            user: {
-              followers: { some: { id: currentUserId } },
+          limit,
+          ctx,
+          cursor,
+          whereClause:
+            currentUserId == null || !onlyFollowing ? undefined : {
+              user: {
+                followers: { some: { id: currentUserId } },
+              },
             },
-          },
         });
-
-        // let nextCursor: typeof cursor | undefined
-
-        // if (data.length > limit) {
-        //   const nextItem = data.pop()
-        //   if (nextItem != null) {
-        //     nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt }
-        //   }
-        // }
-
-        // return {
-        //   tweets: data.map(tweet => {
-        //     return {
-        //       id: tweet.id,
-        //       content: tweet.content,
-        //       createdAt: tweet.createdAt,
-        //       likeCount: tweet._count.likes,
-        //       user: tweet.user,
-        //       likedByMe: tweet.likes?.length > 0, // you've liked this
-        //     }
-        //   }), nextCursor
-
       }
     ),
+
+  // let nextCursor: typeof cursor | undefined
+
+  // if (data.length > limit) {
+  //   const nextItem = data.pop()
+  //   if (nextItem != null) {
+  //     nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt }
+  //   }
+  // }
+
+  // return {
+  //   tweets: data.map(tweet => {
+  //     return {
+  //       id: tweet.id,
+  //       content: tweet.content,
+  //       createdAt: tweet.createdAt,
+  //       likeCount: tweet._count.likes,
+  //       user: tweet.user,
+  //       likedByMe: tweet.likes?.length > 0, // you've liked this
+  //     }
+  //   }), nextCursor
+
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
