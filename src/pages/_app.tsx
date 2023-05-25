17f78c99a467124dcfe538c44d4db924491aa7d8
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 03:55:33 -0400

    log in and out - done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
+import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +14,26 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Bark</title>
+        <meta 
+        name="description" 
+        content="Bark Now!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div 
+      className="
+      container 
+      mx-auto 
+      flex
+      items-start
+      ">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
       <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
