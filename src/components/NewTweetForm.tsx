dfbf448ceab2f6eb56210cb48764c2e77abb44a3
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 05:07:17 -0400

    fixed NewTweetForm

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -12,7 +12,7 @@ function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
 
 export function NewTweetForm() {
     const session = useSession();
-    if (session.status !== "authenticated") return;
+    if (session.status !== "authenticated") return null;
 
     return <Form />
 }
