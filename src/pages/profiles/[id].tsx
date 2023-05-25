f10a8058a0bab151df632b093c3e905a094441d2
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 11:53:08 -0400

    removed errant semicolon

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -110,7 +110,7 @@ const pluralRules = new Intl.PluralRules()
 
 function getPlural(number: number, singular: string, plural: string) {
     return pluralRules.select(number) === "one" ? singular : plural
-};
+}
 
 export const getStaticPaths: GetStaticPaths = () => {
     return {
