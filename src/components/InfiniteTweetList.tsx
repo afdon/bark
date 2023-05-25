f349c795f1aa9b95e51e6b4d5db8c3134d28f6a7
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 06:28:02 -0400

    heart icons from react-icons

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -98,12 +98,21 @@ function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
         );
     }
 
-
-
     return (
-        <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
-            <HeartIcon />
-            <span>{likeCount}</span>
-        </div>
+        <button
+        className={`group items-center gap-1 self-start flex transition-colors duration-200 ${
+            likedByMe 
+            ? "text-red-500"
+            : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+        }`}
+        >
+            <HeartIcon className={`transition-colors duration-200 ${
+                likedByMe 
+                ? "fill-red-500" 
+                : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+            }`} 
+                />
+                <span>{likeCount}</span>
+        </button>
     );
 }
