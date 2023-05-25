216b0eed2f78f304bc5ce7a2a2b612b5426f2eb7
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 05:05:28 -0400

    new tweets created

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,7 +1,8 @@
 import { useSession } from "next-auth/react";
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
+import { api } from "~/utils/api";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
     if (textArea == null) return
@@ -17,30 +18,47 @@ export function NewTweetForm() {
 }
 
 function Form() {
-        const session = useSession();
-        const [inputValue, setInputValue] = useState('');
-        const textAreaRef = useRef<HTMLTextAreaElement>();
-        const InputRef = useCallback((textArea: HTMLTextAreaElement) => {
-            updateTextAreaSize(textArea)
-            textAreaRef.current = textArea
-        }, []);
-        
-        useLayoutEffect(() => {
-            updateTextAreaSize(textAreaRef.current);
-        }, [inputValue]);
-        
-        if (session.status !== "authenticated") return null;
-        
-        return (
-            <form className="flex flex-col gap-2 border-b px-4 py-2">
+    const session = useSession();
+    const [inputValue, setInputValue] = useState('');
+    const textAreaRef = useRef<HTMLTextAreaElement>();
+    const InputRef = useCallback((textArea: HTMLTextAreaElement) => {
+        updateTextAreaSize(textArea)
+        textAreaRef.current = textArea
+    }, []);
+
+    useLayoutEffect(() => {
+        updateTextAreaSize(textAreaRef.current);
+    }, [inputValue]);
+
+
+    const createTweet = api.tweet.create.useMutation({
+        onSuccess: newTweet => {
+            console.log("newTweet: ", newTweet);
+            setInputValue("")
+        },
+    });
+
+
+    if (session.status !== "authenticated") return null;
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault()
+
+        createTweet.mutate({ content: inputValue })
+    }
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-2 border-b px-4 py-2">
             <div className="flex gap-4">
                 <ProfileImage src={session.data.user.image} />
-                <textarea 
-                ref={InputRef}
-                style={{ height: 0 }}
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)}
-                className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none " placeholder="What's up, dog?"/>
+                <textarea
+                    ref={InputRef}
+                    style={{ height: 0 }}
+                    value={inputValue}
+                    onChange={e => setInputValue(e.target.value)}
+                    className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none " placeholder="What's up, dog?" />
             </div>
             <Button className="self-end">
                 tweet
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -9,8 +9,10 @@ export const tweetRouter = createTRPCRouter({
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async({ input: { content }, ctx}) => {
-      return await ctx.prisma.tweet.create({ 
+      const tweet = await ctx.prisma.tweet.create({ 
         data: { content, userId: ctx.session.user.id },
       });
+
+      return tweet;
     }),
 });
