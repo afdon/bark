2cffd006922691feea4aac5739354e99700f7ab8
afdon <53676753+afdon@users.noreply.github.com>
2023-05-25 06:08:13 -0400

    rendering dates with datetimeformatter

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -45,6 +45,8 @@ export function InfiniteTweetList({ tweets, isError, isLoading, fetchNewTweets, 
     );
 }
 
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, { dateStyle: "short" })
+
 function TweetCard({ 
     id, 
     user, 
@@ -54,6 +56,22 @@ function TweetCard({
     likedByMe,
 }: Tweet) {
     return (
-        <li className="flex gap-4 border-b px-4 py-4"><Link href={`/profiles/${user.id}`}><ProfileImage src={user.image} /></Link></li>
+        <li className="flex gap-4 border-b px-4 py-4">
+            <Link href={`/profiles/${user.id}`}>
+                <ProfileImage src={user.image} />
+            </Link>
+            <div className="flex flex-grow flex-col">
+                <div className="flex gap-1">
+                    <Link 
+                    href={`/profiles/${user.id}`}
+                    className="font-bold outline:none hover:underline focus-visible:underline"
+                    >
+                        {user.name}
+                    </Link>
+                    <span className="text-gray-500">-</span>
+                    <span className="text-gray-500">{dateTimeFormatter.format(createdAt)}</span>
+                </div>
+            </div>
+            </li>
     );
 }
